96c319c671c9d9814f96c6425be0557621c72e71
KenTasaki <tazaki@startup-technology.com>
2019-08-01 19:33:07 +0900

    feat:新規追加用のコンポーネント(card/list共用)を追加

--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import TrelloCard from './TrelloCard'
+import TrelloCard from './TrelloCard';
+import TrelloActionButton from './TrelloActionButton';
 
 const TrelloList = ({title, cards}) => {
   return (
-    <div style={style.container}> 
+    <div style={style.container}>
       <h4>{title}</h4>
-      { cards.map(card => (
-        <TrelloCard text={card.text}/>
-      ))}
+      {cards.map (card => <TrelloCard key={card.id} text={card.text} />)}
+      <TrelloActionButton />
     </div>
   );
-}
+};
 
-const style ={
+const style = {
   container: {
     backgroundColor: '#dfe3e6',
     borderRedius: 3,
     width: 300,
+    height: '100%',
     padding: 8,
-    marginRight: 8
-  }
-}
+    marginRight: 8,
+  },
+};
 
 export default TrelloList;
