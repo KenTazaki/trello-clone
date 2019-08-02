f711f1700ccf0f745dfbe9e3fb88404fcf462fb7
KenTasaki <tazaki@startup-technology.com>
2019-08-02 15:04:20 +0900

    feat:新規リスト/カードの追加メソッド

--- src/reducers/listsReducer.js
+++ src/reducers/listsReducer.js
@@ -1,39 +1,70 @@
+import {CONSTANTS} from '../actions';
+
+let listID = 2;
+
 const initialState = [
   {
-    title: "前回のあらすじ",
+    title: '前回のあらすじ',
     id: 0,
     cards: [
       {
         id: 0,
-        text: "reducerしようぜ"
+        text: 'reducerしようぜ',
       },
       {
         id: 1,
-        text: "マテリアルUI使おうぜ"
-      }
-    ]
+        text: 'マテリアルUI使おうぜ',
+      },
+    ],
   },
   {
-    title: "今回の話",
+    title: '今回の話',
     id: 1,
     cards: [
       {
         id: 0,
-        text: "色々やろうぜ"
+        text: '色々やろうぜ',
       },
       {
         id: 1,
-        text: "どんどんやろうぜ"
-      }
-    ]
-  }
-]
+        text: 'どんどんやろうぜ',
+      },
+    ],
+  },
+];
 
 const listsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CONSTANTS.ADD_LIST:
+      const newList = {
+        title: action.payload,
+        cards: [],
+        id: listID,
+      };
+      listID += 1;
+      return [...state, newList];
+    case CONSTANTS.ADD_CARD:
+      const cardID = state.find (list => list.id === action.payload.listID)
+        .cards.length;
+      const newCard = {
+        text: action.payload.text,
+        id: cardID,
+      };
+
+      const newState = state.map (list => {
+        if (list.id === action.payload.listID) {
+          return {
+            ...list,
+            cards: [...list.cards, newCard],
+          };
+        } else {
+          return list;
+        }
+      });
+      return newState;
     default:
       return state;
   }
-}
+};
 
 export default listsReducer;
